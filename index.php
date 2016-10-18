af3151f85ad5706d1df86169dbdf6b4d54aa55d0
Bikramjeet S Sandhu <bss46@afsconnect1.njit.edu>
2016-10-17 22:53:29 -0400

    added index page with nav bar and logo

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<center><h1>Welcome to Bikram Sandhu's page!</h1></center>
+			<style>
+				ul {
+					list-style-type:none;
+					margin: 0;
+					padding: 0;
+					overflow: hidden;
+					background-color: #333;
+				}
+				li {
+					float: center;
+				}
+				li a {
+					display: block;
+					color:white;
+					text-align:center;
+					padding: 14px 16px;
+					text-decoration: none;
+				}
+				li a:hover{
+				background-color: #111;
+				}
+			</style>
+	</head>
+		<body bgcolor="#808080">
+	<div>
+	<ul>
+		<li><a href="signup.php">Sign Up Today</a></li>
+		<li><a href="login.php">Login</a></li>
+	</ul>
+	</div>
+	<br><br><br><br>
+	<center><img src="http://www5.njit.edu/summer/sites/summer/files/styles/large/public/NJIT.jpg?itok=0jqKS40d" alt="NJIT"></center>
+
+		</body>
+</html>
