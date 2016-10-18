40f16a5b0e5a0e0f41cfa249033f0175bea6ed51
bikramsandhu <bss46@njit.edu>
2016-10-18 00:31:00 -0300

    Created SignUp.php since git on putty is not working for me

---
+++ SignUp.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+        <head>
+        <title>Sign up for our website today!</title>
+        <style type = "text/css">
+                h1{
+                position:relative;
+                top:0px;
+                color:red;
+                }
+                input.first{
+                position:relative;
+                top:30px;
+                width:300px;
+                padding: 10px;
+                }
+                input.last{
+                position:relative;
+                top:60px;
+                width:300px;
+                padding:10px;
+                }
+                input.user{
+                position:relative;
+                top:90px;
+                width:300px;
+                padding:10px;
+                }
+                input.email{
+                position:relative;
+                top:120px;
+                width:300px;
+                padding:10px;
+                }
+                input.pass{
+                position:relative;
+                top:150px;
+                width:300px;
+                padding:10px;
+                }
+                input.conf{
+                position:relative;
+                top:180px;
+                width:300px;
+                padding:10px;
+                }
+                input.phone{
+                position:relative;
+                top:210px;
+                width:300px;
+                padding:10px;
+                }
+        </style>
+        </head>
+    <body bgcolor="#3E0D6B">
+                <img src="http://archives.njit.edu/vhlib/images/njit_logo.gif" alt="NJIT logo">
+                <form action="form.php" method="post">
+                <div>
+                <center><h1>Sign up for our website today!</h1></center>
+                </div>
+                <div>
+                <center><input placeholder="First Name" type="text"     class="first"/></center>
+                </div>
+                <div>
+                <center><input placeholder="Last Name" type="text"     class="last"/></center>
+                </div>
+                <div>
+                <center><input placeholder="Pick a username" type="text"     class="user"/></center>
+                </div>
+                <div>
+                <center><input placeholder="E-mail Address" type="text"     class="email"/></center>
+                </div>
+                <div>
+                <center><input placeholder="Pick a password" type="password"     class="pass"/></center>
+                </div>
+                <div>
+                <center><input placeholder="Confirm Password" type="password"     class="conf"/></center>
+                </div>
+                <div>
+                <center><input placeholder="Phone Number" type="tel"    class="phone"/></center>
+                </div>
+                <br><br><br><br><br><br><br><br><br><br><br><br><br>
+                <div>
+                <center><input type="submit"    value="Sign Up!"></center>
+                </div>
+                <br><br><br>
+                <div>
+                <center><input type="button" value="Go back home"
+                onclick="window.location.href='http://web.njit.edu/~bss46/forms/index.php'"/></center>
+                </div>
+                </form>
+                </body>
+</html>
